4cff4f7217169093e46958b61111abde3da6ea3b
DylanM540 <dylanmaurcot5pv@gmail.com>
2022-07-22 22:53:16 +0200

    Exercice 1 and 2

---
+++ 2.1.js
@@ -0,0 +1,10 @@
+
+let aNumber = Number(prompt("Quelles est votre age ?"));
+
+
+if (aNumber >= 18) {
+  console.log("Vous êtes un adulte");
+} else {
+  console.log("Vous n'êtes pas encore majeur");
+}
+
