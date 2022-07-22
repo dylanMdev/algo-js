b13fbb17d1b328e5398e112d7760c7d1afcd1374
DylanM540 <dylanmaurcot5pv@gmail.com>
2022-07-22 22:54:22 +0200

    Exercice 1 and 2

---
+++ 2.3.js
@@ -0,0 +1,22 @@
+for (let i = 2; i <= 100; i += 2)
+{
+    console.log(i);
+}
+
+
+
+
+let j = 2; 
+
+while (j <= 100) {
+    console.log(j++);
+  }
+
+
+
+  /*
+// BONUS //
+for(let i = 1; i <= 50; i++)
+{
+    console.log(i * 2);
+} */
