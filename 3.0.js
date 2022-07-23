56f25db06c5a345c610fc0d6a7795349fd1601fb
DylanM540 <dylanmaurcot5pv@gmail.com>
2022-07-23 12:35:05 +0200

    Exercice 2 and 3

---
+++ 3.0.js
@@ -0,0 +1,27 @@
+let learner = [
+    "Baudson Guillaume",
+    "Benaccetta Rinaldo",
+    "Blaevoet Benoit",
+    "Brigode Xavier",
+    "Charles Xavier",
+    "Corda Daniela",
+    "Denis Anthony",
+    "Devilers Vincent",
+    "Elinckx Julien",
+    "Elyahyioui Selim",
+    "Friquet Luca",
+    "Gillard Sophie",
+    "Kirac Axel",
+    "Maddouri Hazem",
+    "Maurcot Dylan",
+    "Moussiaux Titouan",
+    "Moraes Kamilla",
+    "Pécher Stéphanie",
+    "Scourneau Julien",
+    "Vanvolcksom Doriano",
+    "Vervoot Eddy"
+];
+for(let item of learner)
+{
+    console.log(item);
+}
